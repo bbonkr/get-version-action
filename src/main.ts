470fba668b3c47a79196dcc0eb94174d98b296e8
Pon Cheol Ku (구본철) <bbon@live.com>
2022-01-18 17:29:19 +0900

    Fix path

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import * as core from '@actions/core'
+import * as glob from '@actions/glob'
 import {getVersion} from './getVersion'
 import {inputs} from './inputs'
 import {outputs} from './outputs'
@@ -7,7 +8,14 @@ async function run(): Promise<void> {
   try {
     const project = core.getInput(inputs.project)
 
-    const result = await getVersion({project})
+    const globber = await glob.create(project)
+    const files = await globber.glob()
+    const projectFile = files.find((_, index) => index === 0)
+
+    if (!projectFile) {
+      throw new Error('Does not provide exists file path')
+    }
+    const result = await getVersion({project: projectFile})
 
     core.setOutput(outputs.version, result)
   } catch (error) {
